280343264f78d4ea5b4770f8033036be78c78c47
Hithan-01 <1200455@alumno.um.edu.mx>
2025-12-08 17:07:01 -0600

    Creacion del proyecto

---
+++ backend/src/test/java/com/example/laras/LarasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LarasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
